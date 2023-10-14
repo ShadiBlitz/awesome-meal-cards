a82aa7d8ade995cdb8fdc145f7381d39988fd072
ShadiBlitz <101566855+ShadiBlitz@users.noreply.github.com>
2023-10-14 13:44:43 +0300

    style: fix existing code formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import { Metadata } from "next/types";
-import "./globals.css";
+import { Metadata } from 'next/types'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Awesome Meal Cards",
-};
+  title: 'Awesome Meal Cards',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
-  );
+  )
 }
