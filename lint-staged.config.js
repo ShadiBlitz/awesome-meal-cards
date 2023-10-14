eb00e94d06b456ca2d21924d84ec234f26e7f267
ShadiBlitz <101566855+ShadiBlitz@users.noreply.github.com>
2023-10-14 14:23:51 +0300

    chore: add ts type checking and linting in a precommit hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  // this will run type checks on Typescript files
+  '{app}/**/*.{ts,tsx}': () => 'pnpm type:check',
+
+  // This will lint staged javascript files
+  '{app}/**/*.{ts,tsx,js,jsx}': (files) => [`pnpm lint ${files.join(' ')}`],
+
   // This will format staged javascript files
   '*.{js,jsx,ts,tsx}': (files) => [`pnpm format ${files.join(' ')}`],
 }
